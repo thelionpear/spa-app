fe2c80a8f12cf2d2c15c7d32069ff1f4090477a0
Michelle Garcia <thelionpear@gmail.com>
2018-06-13 13:56:58 -0600

    final touches

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -4,6 +4,11 @@ class Form extends React.Component {
   defaultValues ={ name: '', price: '', description: '', department: '' }
   state = {...this.defaultValues}
 
+  componentDidMount() {
+    if (this.props.id)
+      this.setState({ ...this.props })
+  }
+
   handleSubmit = (e) => {
     e.preventDefault()
     this.props.submit(this.state)
--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,28 +1,61 @@
-import React from 'react' 
-import axios from 'axios' 
+import React from 'react'
+import axios from 'axios'
+import Form from './Form'
 
 class Product extends React.Component {
-    state = { product: {} }
+  state = { product: {}, edit: false }
 
-    componentDidMount() {
-        const { id } = this.props.match.params
-        axios.get( `/api/products/${id}`)
-            .then( ({ data }) => this.setState({ product: data }) )
-    }
+  componentDidMount() {
+    const { id } = this.props.match.params
+    axios.get(`/api/products/${id}`)
+      .then( ({ data }) => this.setState({ product: data }) )
+  }
 
-    render() {
-        const { product: { name, description, price, department}
-        } = this.state
-        return (
-            <div>
-                <h1>{name}</h1>
-                <h3>{description}</h3>
-                <h3>${price}</h3>
-                <h3>{department}</h3>
-            </div> 
-        )
-        }
-    }
+  toggleEdit = () => {
+    this.setState( state => {
+      return { edit: !state.edit }
+    })
+  }
 
+  submit = (product) => {
+    const { id } = this.state.product
+    axios.put(`/api/products/${id}`, { product })
+    .then( ({ data }) => {
+      this.setState({ product: data, edit: false }) 
+    })
+  }
 
-export default Product 
+  show() {
+    const { 
+      product: { name, description, price, department }
+    } = this.state
+    return (
+      <div>
+        <h1>{name}</h1>
+        <h3>{description}</h3>
+        <h3>${price}</h3>
+        <h3>{department}</h3>
+      </div>
+    )
+  }
+
+  edit = () => {
+    const { id, department, description, price } = this.state.product 
+    const product = { id, department, description, price }
+    return <Form submit={this.submit} {...product} />
+  }
+
+  render() {
+    const { edit } = this.state;
+    return (
+      <div>
+        { edit ? this.edit() : this.show() }
+        <button onClick={this.toggleEdit}>
+          { edit ? 'Cancel' : 'Edit' }
+        </button>
+      </div> 
+    )
+  }
+}
+
+export default Product
