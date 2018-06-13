e8b5c7b4c4a05f69b9fa82bf78a0533a44a1ae3e
Michelle Garcia <thelionpear@gmail.com>
2018-06-13 09:51:12 -0600

    faker and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do 
+    Product.create(
+        name: Faker::Commerce.product_name, 
+        description: Faker::Hipster.sentence, 
+        price: Faker::Commerce.price.to_f,
+        department: Faker::Commerce.department
+    )
+end 
