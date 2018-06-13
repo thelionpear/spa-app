412a1bace5849e85c0f2e0aa45b9c92dfc887219
Michelle Garcia <thelionpear@gmail.com>
2018-06-13 13:30:46 -0600

    product page

---
+++ client/src/components/Form.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+class Form extends React.Component {
+  defaultValues ={ name: '', price: '', description: '', department: '' }
+  state = {...this.defaultValues}
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.submit(this.state)
+    this.setState({...this.defaultValues})
+  }
+
+  handleChange = (e) => {
+    const { target: { name, value } } = e
+    this.setState({ [name]: value })
+  }
+
+  render() {
+    const { name, price, description, department } = this.state
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          name="name"
+          placeholder="name"
+          value={name}
+          onChange={this.handleChange}
+          required
+        />
+        <input
+          name="description"
+          placeholder="description"
+          value={description}
+          onChange={this.handleChange}
+        />
+        <input
+          name="price"
+          placeholder="price"
+          type="number"
+          value={price}
+          onChange={this.handleChange}
+        />
+         <input
+          name="department"
+          placeholder="Department"
+          value={department}
+          onChange={this.handleChange}
+        />
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
+
+export default Form
