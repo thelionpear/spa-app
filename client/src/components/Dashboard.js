5bf763c1b15c7f6e554d44af26ca4b8470a0e970
Michelle Garcia <thelionpear@gmail.com>
2018-06-13 11:44:14 -0600

    reworking the dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Redirect } from 'react-router-dom'
 import { isAuthenticated } from '../fakeAuth'
+import axios from 'axios'
 
-const Dashboard = () => {
-    if (isAuthenticed())
-        return <h3>You are logged in</h3>
-    else 
-        return <Redirect to="/login" /> 
+class Dashboard extends React.Component {
+    state = { products: [] } 
+
+    componentDidMount() {
+        axios.get('/api/products')
+            .then( res => this.setState({ products: res.data }))
+    }
 }
 
 export default Dashboard 
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,20 +1,48 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom' 
+import React, { Fragment } from 'react'
+import { NavLink, withRouter } from 'react-router-dom'
+import { isAuthenticated, logout } from '../fakeAuth'
 
 const styles = {
-    active: {
-        textDecoration: 'none', 
-        fontWeight: 'bold', 
-        color: 'black' 
-    }
+  active: {
+    textDecoration: 'none',
+    fontWeight: 'bold',
+    color: 'black'
+  },
+  link: {
+    textDecoration: 'underline',
+    color: 'blue',
+    cursor: 'pointer',
+  }
 }
 
-const NavBar = () => (
-    <nav>
-        <NavLink exact activeStyle={styles.active} to="/">Home</NavLink>
-        {' '}
-        <NavLink activeStyle={styles.active} to="/about">About</NavLink>
-    </nav>
+const NavBar = ({ history }) => (
+  <nav>
+    <NavLink exact activeStyle={styles.active} to="/">Home</NavLink>
+    {' '}
+    <NavLink activeStyle={styles.active} to="/about">About</NavLink>
+    { isAuthenticated() ?
+        <Fragment>
+          {' '}
+          <NavLink activeStyle={styles.active} to="/dashboard">
+            Dashboard
+          </NavLink>
+          {' '}
+          <span style={styles.link} onClick={ () => {
+            logout()
+            history.push("/login")
+          }}>
+            Logout
+          </span>
+        </Fragment>
+        :
+        <Fragment>
+          {' '}
+          <NavLink activeStyle={styles.active} to="/login">
+            Login
+          </NavLink>
+        </Fragment>
+    }
+  </nav>
 )
 
-export default NavBar 
+export default withRouter(NavBar)
